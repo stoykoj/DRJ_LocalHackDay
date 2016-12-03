b55f013cad2c574f4f67c3c2dc6b5d83120bb247
Liam Sparling <mrbiam@users.noreply.github.com>
2016-12-03 14:31:31 -0600

    Did create account checks

--- app/src/main/java/com/julia/drj_localhackday2016/CreateAccount.java
+++ app/src/main/java/com/julia/drj_localhackday2016/CreateAccount.java
@@ -4,11 +4,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
+
+
 /**
  * Created by Constellations on 2016-12-03.
  */
 
 public class CreateAccount extends AppCompatActivity {
+    EditText email;
+    EditText username;
+    EditText password;
+    EditText confirmPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,17 +24,13 @@ public class CreateAccount extends AppCompatActivity {
     }
 
     public void confirmCreate(View view) {
-        String email = findViewById(R.id.emailIn).toString();
-        String username = findViewById(R.id.usernameIn).toString();
-        String password = findViewById(R.id.passwordIn).toString();
-        String confirmPass = findViewById(R.id.confirmPassIn).toString();
-        if (0) {
-
-        }
-        else {
-            Intent intent = new Intent(this, MainScreen.class);
-            startActivity(intent);
-        }
+            email = (EditText)findViewById(R.id.emailIn);
+            username = (EditText)findViewById(R.id.usernameIn);
+            password = (EditText)findViewById(R.id.passwordIn);
+            confirmPass = (EditText)findViewById(R.id.confirmPassIn);
+            if ((password.getText().toString()).equals(confirmPass.getText().toString()) && (email.getText() != null) && (username.getText() != null)) {
+                Intent intent = new Intent(this, MainScreen.class);
+                startActivity(intent);
+            }
     }
 }
-
