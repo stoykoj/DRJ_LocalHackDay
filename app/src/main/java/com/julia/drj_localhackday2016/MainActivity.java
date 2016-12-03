44d2a1c48bfe6cbc59386e7cddb2669201b5ca87
kimlaberinto <kim.laberinto@gmail.com>
2016-12-03 14:00:42 -0600

    Migrated from ActionBar to Toolbar

--- app/src/main/java/com/julia/drj_localhackday2016/MainActivity.java
+++ app/src/main/java/com/julia/drj_localhackday2016/MainActivity.java
@@ -3,7 +3,9 @@ package com.julia.drj_localhackday2016;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.galleryActToolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void signin(View view){
