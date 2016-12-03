4efd27008f0a656ca9941b61e00b625d325f7158
Julia Stoyko <stoykojulia@gmail.com>
2016-12-03 14:33:54 -0600

    Checks if passwords match

--- app/src/main/java/com/julia/drj_localhackday2016/CreateAccount.java
+++ app/src/main/java/com/julia/drj_localhackday2016/CreateAccount.java
@@ -4,29 +4,46 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.Toast;
+
 /**
  * Created by Constellations on 2016-12-03.
  */
 
 public class CreateAccount extends AppCompatActivity {
+    private EditText email, username, pw, confirmpw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
+        email = (EditText)findViewById(R.id.emailIn);
+        username = (EditText)findViewById(R.id.usernameIn);
+        pw = (EditText)findViewById(R.id.passwordIn);
+        confirmpw = (EditText)findViewById(R.id.confirmPassIn);
     }
 
-    public void confirmCreate(View view) {
-        String email = findViewById(R.id.emailIn).toString();
-        String username = findViewById(R.id.usernameIn).toString();
-        String password = findViewById(R.id.passwordIn).toString();
-        String confirmPass = findViewById(R.id.confirmPassIn).toString();
-        if (0) {
+    public void confirmCreate(View view){
+        boolean confirmed = true;
 
+        if (pw.getText().toString() != null && confirmpw.getText().toString() != null && pw.getText().toString().equals(confirmpw.getText().toString())){
+            confirmed = true;
+            /*Intent intent = new Intent(this, MainScreen.class);
+            startActivity(intent);*/
         }
-        else {
+        else{
+            Toast.makeText(getApplicationContext(), "Passwords do not match", Toast.LENGTH_SHORT).show();
+            confirmed = false;
+            pw.setText("");
+            confirmpw.setText("");
+        }
+        if (confirmed){
             Intent intent = new Intent(this, MainScreen.class);
             startActivity(intent);
         }
+
     }
+
+
 }
 
